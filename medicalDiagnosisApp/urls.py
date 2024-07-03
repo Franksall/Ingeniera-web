6b0d794c65d6342a3be6563218c2ba85663ee3fb
Frank Duarte <frnk.duor@gmail.com>
2024-07-02 23:46:29 -0500

    Re-upload project files

---
+++ medicalDiagnosisApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('malaria_diagnose/', views.malaria_diagnose, name='malaria_diagnose'),
+    path('pneumonia_diagnose/', views.pneumonia_diagnose, name='pneumonia_diagnose'),
+]
